26d33efdf0d6a21c0021ec5dd324847994430a7e
Sula Fernandes <fernandess.2015@alunos.utfpr.edu.br>
2022-12-08 19:45:02 -0300

    Criação da tela de jogo fase1

---
+++ SaladaDeFrutasAlfabetica/Assets/Scripts/BotaoIniciar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotaoIniciar : MonoBehaviour
+{
+    public  void abrirScene(string nome)
+    {
+        SceneManager.LoadScene(nome);
+    }
+        
+   
+}
