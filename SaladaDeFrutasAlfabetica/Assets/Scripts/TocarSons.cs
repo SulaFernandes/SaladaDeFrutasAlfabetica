4732908206735a0c63dcc79b39fcbe5a4959fd30
Sula Fernandes <fernandess.2015@alunos.utfpr.edu.br>
2022-12-11 17:04:17 -0300

    sons

---
+++ SaladaDeFrutasAlfabetica/Assets/Scripts/TocarSons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TocarSons : MonoBehaviour
+
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+
+    public void tocarSonsLetras()
+    {
+        //audioSource.Play();
+        audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+}
