3bf9f78e56c9103d92bca2c6959175b323d313f5
Sula Fernandes <fernandess.2015@alunos.utfpr.edu.br>
2022-12-11 10:11:39 -0300

    Criado controle arrastar e soltar

---
+++ SaladaDeFrutasAlfabetica/Assets/Scripts/ArrastarESoltar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ArrastarESoltar : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler
+{
+    RectTransform rectTransform;
+    CanvasGroup group;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        group = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        group.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        group.blocksRaycasts = true;
+    }
+
+}
---
+++ SaladaDeFrutasAlfabetica/Assets/Scripts/Cesta.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cesta : MonoBehaviour, IDropHandler
+{
+    RectTransform rectTransform;
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag != null)
+        {
+            Debug.Log("oi");
+            
+        }
+    }
+
+    /*public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }*/
+
+
+}
